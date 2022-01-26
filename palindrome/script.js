d0caa0b05d47e1a933efcc695fdddbc497c43f7b
Daniel Phillips <dan.phillips100@gmail.com>
2022-01-26 13:21:06 +0000

    Updated loop efficiency.

--- palindrome/script.js
+++ palindrome/script.js
@@ -1,18 +1,24 @@
 const palindromes = string => {
 
-    // strip out spaces and special characters using regex
-    const newPal = string.replace(/[.,!\s]/g, '').toLowerCase();
-  
-    // get length of input
-    const len = newPal.length-1;
+        // strip out spaces and special characters using regex
+        const newPal = string.replace(/[.,!?\s]/g, '').toLowerCase();
+    
+        // get length of input
+        const len = newPal.length-1;
 
-    // compare first and last characters to see if they match, then step in one if they do, or return false if not
-    for (let i = 0; i < len; i++) {
-      if (newPal[i] != newPal[len-i]) { 
-        return false;
-        } 
-    }
-    return true;
+        // compare first and last characters to see if they match, then step in one if they do, or return false if not
+        for (let i = 0; i < len/2; i++) {
+            if (newPal[i] != newPal[len-i]) { 
+                return false;
+            } 
+        }
+        return true;
   };
 
-  console.log(palindromes('slacalsf'));
+  console.log(palindromes('red rum, sir, is murder'));
+
+  console.log(palindromes('Eva, can I see bees in a cave?'));
+
+  console.log(palindromes('Doc, note, I dissent. A fast never prevents a fatness. I diet on cod.'));
+
+ 
