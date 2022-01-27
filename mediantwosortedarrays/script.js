cc6ed1ff036ee810f83f3101f8f257310eee10a1
Dan Phillips <dan.phillips100@gmail.com>
2022-01-27 17:41:42 +0000

    Corrected average for mean

--- mediantwosortedarrays/script.js
+++ mediantwosortedarrays/script.js
@@ -1,14 +1,26 @@
-const array1 = [1, 2, 5, 6];
-const array2 = [3, 7, 8, 13];
+const array1 = [1,3];
+const array2 = [2, 4];
 
 //merge two arrays and sort into order
 const finalArray = array1.concat(array2);
-finalArray.sort();
 
-console.log(finalArray);
-let count = 0;
+function medianArray(finalArray) {
+    const concat = finalArray;
+    concat - concat.sort(
+        function(a, b) {
+            return a - b;
+        })
 
-for (let i = 0; i < finalArray.length; i++) {
-    count +=finalArray[i];
+    console.log(concat)
+    const len = concat.length;
+
+    if (len % 2 === 1) {
+        console.log(concat[(len / 2) - .5]);
+        return concat[(len / 2) - .5];
+    } else {
+        console.log((concat[len / 2] + concat[(len / 2) - 1]) / 2);
+        return (concat[len / 2] + concat[(len / 2) - 1]) / 2;
+    }
 }
-console.log(count/2)
+
+medianArray(finalArray);
